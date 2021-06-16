a14313d7881533af4e89f056377d8575f1c23eb4
mavimv <maria.ferreira@aluno.faculdadeimpacta.com.br>
2021-06-16 07:39:23 -0300

    Sistema terminou a maioria das custom, falta a visualização op Ainda

---
+++ static/js/home.js
@@ -0,0 +1,9 @@
+var status = document.getElementById('status')
+
+if (status === 'Em andamento'){
+    style="color:orange;"
+}
+
+função divColorChange(){ 
+    document.getElementById('status')style.color = ' # FF0000 ';
+}
